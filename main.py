f89edb3d2a29d610995e5cbe04151f74a73b9025
MochaSm <130510590+MochaSm@users.noreply.github.com>
2025-06-11 13:30:03 -0700

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ RED = (255,0,0)
 YELLOW = (255,255,0)
 
 def end(sec):
+
     win.fill(WHITE)
     text = font.render("Game Over press enter to start again or ESC to leave", True, BLACK)
     score_text = font.render(f"It took you: {sec[0]}s", True, BLACK)
@@ -25,84 +26,104 @@ def end(sec):
     if keys[pygame.K_ESCAPE]:
         pygame.quit()
         sys.exit()
+    pygame.display.update()
         
 def main():
     clock =pygame.time.Clock()
     x,y = 30,50
     speed =10
-    coords =0
     z,w = 0,0
-    
     last_event_time = 0
     circlepos = []
-    
-
-
-  
     Score =0
     start_ticks = pygame.time.get_ticks()
+    
     def rand ():
         w = random.randint(0,480)
         z = random.randint(0,780)
+
         return z ,w
     
     FPS = 60
     z, w = rand()
+    circlepos.append((z, w))
+
     sec.clear()
     pygame.init()
-    
+    running = True
     while True:
-        keys = pygame.key.get_pressed()
+        if running:
+            ran = random.randint(3, 5)
 
-        clock.tick(FPS)
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-        if keys[pygame.K_UP]:
-            if y> 0:
-                y-=speed
-        if keys[pygame.K_DOWN]:
-            if y<480:
-                y+=speed
-        if keys[pygame.K_LEFT]:
-            if x> 0:
-                x-=speed
-        if keys[pygame.K_RIGHT]:
-            if x < 780:
-                x+=speed
-        win.fill(BLACK)
-        player = pygame.Rect(x,y,20,20)
-        seconds= (pygame.time.get_ticks()-start_ticks)/1000
+            keys = pygame.key.get_pressed()
 
-        if seconds - last_event_time >= 1.5:
-            z, w = rand()
-            circlepos.append((z, w))
-            last_event_time = seconds
-
-        for a,b in circlepos:
-            pygame.draw.circle(win,YELLOW,(a,b),10)
-            if player.collidepoint(a,b):
-                Score += 1
-                circlepos.remove((a,b))
-       
-         
-        
-        
-        pygame.draw.rect(win,RED,player)
-        pygame.draw.circle(win,YELLOW,(z,w),10) 
-        
-        if Score >= 10:
-            sec.append(round(seconds,1))
-            end(sec)
-        time = font.render(f"{round(seconds,1)}s", True, WHITE)
-        score_text = font.render(f"Score:{Score}", True, WHITE)
-        win.blit(time, (time.get_width(),time.get_height()))
-        win.blit(score_text, (score_text.get_width()+50,  score_text.get_height() ))
+            clock.tick(FPS)
             
-        pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            if keys[pygame.K_UP]:
+                if y> 0:
+                    y-=speed
+            if keys[pygame.K_DOWN]:
+                if y<480:
+                    y+=speed
+            if keys[pygame.K_LEFT]:
+                if x> 0:
+                    x-=speed
+            if keys[pygame.K_RIGHT]:
+                if x < 780:
+                    x+=speed
+            win.fill(BLACK)
+            player = pygame.Rect(x,y,20,20)
+            seconds= (pygame.time.get_ticks()-start_ticks)/1000
+
+            if seconds - last_event_time >= ran:
+                print(ran)
+                z, w = rand()
+                circlepos.append((z, w))
+                last_event_time = seconds
+
+            for a,b in circlepos:
+                pygame.draw.circle(win,YELLOW,(a,b),15)
+                if player.collidepoint(a,b):
+                    z, w = rand()
+                    circlepos.append((z, w))
+
+                    Score += 1
+                    circlepos.remove((a,b))
+
+            pygame.draw.rect(win,RED,player)
+            if Score >= 1:
+                sec.append(round(seconds,1))
+                running = False
+            time = font.render(f"{round(seconds,1)}s", True, RED)
+            score_text = font.render(f"Score:{Score}", True, WHITE)
+            win.blit(time, (time.get_width(),time.get_height()))
+            win.blit(score_text, (score_text.get_width()+50,  score_text.get_height() ))
+            pygame.display.update()
+        else:
+            end(sec)
+            # win.fill(WHITE)
+            # text = font.render("Game Over press enter to start again or ESC to leave", True, BLACK)
+            # score_text = font.render(f"It took you: {sec[0]}s", True, BLACK)
+            # win.blit(text, (WIDTH//2 - text.get_width()//2, HEIGHT//2 - text.get_height()//2))
+            # win.blit(score_text, (WIDTH//2 - score_text.get_width()//2, HEIGHT//2 - score_text.get_height()//2 + 100))
+            # pygame.display.update()
+
+            # keys = pygame.key.get_pressed()
+
+            # if keys[pygame.K_KP_ENTER] or keys[pygame.K_RETURN]:
+            #     main()
+            #     pygame.display.flip()
+
+            # if keys[pygame.K_ESCAPE]:
+            #     pygame.quit()
+            #     sys.exit()
+            #     pygame.display.update()
+
+       
     
 if __name__ == "__main__":
-
     main()
